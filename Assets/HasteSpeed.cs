aa73a62ed51ea645f6ff7332c7731b8a0ff6ea2d
Jason Cothran <healingfox115@gmail.com>
2021-04-18 13:17:31 -0500

    Applied scripts to haste power up
    haste power up speeds up time (opposite of slow down script)

---
+++ Assets/HasteSpeed.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HasteSpeed : MonoBehaviour
+{
+        private AudioSource HasteCollect ;
+    //public ParticleSystem pickupEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HasteCollect = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            HasteCollect.Play();
+            Level01Controller.SetCurrentTime(2.0f);
+            StartCoroutine("Destroy");
+        }       
+    }
+        IEnumerator Destroy()
+    {
+        Renderer[] rs = GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rs)
+        r.enabled = false;
+        Debug.Log("Gotta go fast!");
+        yield return new WaitForSeconds(25f);
+        Level01Controller.SetCurrentTime(1f);
+        gameObject.SetActive(false);        
+    }
+}
