95d4c71174ece6d7b9648a265beeae41aa30b111
Jason Cothran <healingfox115@gmail.com>
2021-05-06 13:03:55 -0500

    Added HP sprite and enlarged box colliders
    Added in the new HP pack sprite and made it easier for the player to walk into the power up.

---
+++ Assets/Scripts/Level01Scripts/Moving/Rotation_hp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//written by Jason
+//script is supposed to give power-ups a visual indicator for the player
+public class Rotation_hp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //simple function makes power up sprite rotate
+        transform.Rotate(new Vector3(0f, 0f, 125f) * Time.deltaTime);
+    }
+}
